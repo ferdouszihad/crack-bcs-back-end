68371c36c44d3595c897e9f4d495dcd36556437e
Your Name~ <f.zihad333@gmail.com>
2022-10-27 13:14:58 +0600

    Offer API added

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const courses = require("./data.json");
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
+
+const offers = require("./offer.json");
+const offeredCourse = [];
+for (const offer of offers) {
+  const matchedCourse = courses.find((course) => course.id === offer.id);
+  matchedCourse["discount"] = offer.discount;
+  offeredCourse.push(matchedCourse);
+}
+app.get("/offers", (req, res) => {
+  res.send(offeredCourse);
+});
+
 app.listen(port, () => {
   console.log("server is running on port: ", port);
 });
